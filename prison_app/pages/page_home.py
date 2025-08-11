5dda6548d2c361e178f21cf840ae9ec3ecca582f
Yasin <101049787+Yasinaslann@users.noreply.github.com>
2025-08-11 17:20:31 +0300

    Update page_home.py

--- prison_app/pages/page_home.py
+++ prison_app/pages/page_home.py
@@ -6,4 +6,7 @@ def app():
     **Prison Predict App**'e hoş geldiniz!  
     Bu uygulama, cezaevi verilerini analiz ederek tahminler yapar, öneriler sunar ve modelin detaylı analizlerini gösterir.
     """)
-    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Prison_cells_in_Helsinki.jpg/640px-Prison_cells_in_Helsinki.jpg", caption="Temsili Cezaevi Görseli")
+    st.image(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Prison_cells_in_Helsinki.jpg/640px-Prison_cells_in_Helsinki.jpg",
+        caption="Temsili Cezaevi Görseli"
+    )
